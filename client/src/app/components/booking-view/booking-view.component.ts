1e23abfdf527cd2dfd6e84586d58696e0524c0ca
Debabratapal <paldebabrata82@gmail.com>
2019-03-18 00:15:39 +0530

    ui booking

--- client/src/app/components/booking-view/booking-view.component.ts
+++ client/src/app/components/booking-view/booking-view.component.ts
@@ -9,21 +9,26 @@ import { timeout } from 'q';
 export class BookingViewComponent implements OnInit {
   times = [];
 
-  rooms = [
+  rooms:any = [
     {
-      room_name: "room_1"
+      room_name: "room_1",
+      booking_times: [10,11, 14,16]
     },
     {
-      room_name: "room_16"
+      room_name: "room_16",
+      booking_times: [10,11, ,16, 18]
     },
     {
-      room_name: "room_14"
+      room_name: "room_14",
+      booking_times: [10,11, 15,16]
     },
     {
-      room_name: "room_16"
+      room_name: "room_16",
+      booking_times: [10,11, 15,16]
     },
     {
-      room_name: "room_431"
+      room_name: "room_431",
+      booking_times: [10,11, 20,16]
     },
   ]
   constructor() { }
@@ -32,6 +37,21 @@ export class BookingViewComponent implements OnInit {
     for(let i=10; i<21; i++) {
       this.times.push(`${i}:00`);
     }
+    this.rooms.forEach((el, i) => {
+      let timings = el.booking_times.sort();
+        el.booking_times = [];
+      for(let j = 10; j<21; j++) {
+        
+        if(timings.includes(j)) {
+          el.booking_times.push(true);
+        } else {
+          el.booking_times.push(false);
+        }
+      }
+    }) 
+   
+    console.log(this.rooms);
+    
   }
 
 }
